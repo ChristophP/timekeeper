e1212c0c22daeb5a2611c873dd3cd53d0ebb7877
Dido (Christoph Poelt) <christoph.poelt@valiton.com>
2026-04-19 20:26:23 +0200

    update to tauri v2

--- src/storage.ts
+++ src/storage.ts
@@ -1,12 +1,12 @@
-import { Store } from "tauri-plugin-store-api";
+import { LazyStore } from "@tauri-apps/plugin-store";
 import dayjs from "./dayjs";
 
 const now = dayjs();
 const currentYearStr = `${now.year()}`;
 const previousYearStr = `${now.subtract(1, "year").year()}`;
 
-const settings = new Store("time-keeper/settings.json");
-const currentYear = new Store(`time-keeper/${currentYearStr}.json`);
-const previousYear = new Store(`time-keeper/${previousYearStr}.json`);
+const settings = new LazyStore("time-keeper/settings.json");
+const currentYear = new LazyStore(`time-keeper/${currentYearStr}.json`);
+const previousYear = new LazyStore(`time-keeper/${previousYearStr}.json`);
 
 export { settings, currentYear, previousYear };
